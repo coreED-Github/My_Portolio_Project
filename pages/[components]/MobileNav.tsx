9dfdedd72cf0855cda6fd876212d508eb168e615
Saira Nasir <sairanasir853@gmail.com>
2024-11-10 10:05:08 -0800

    first commit

--- pages/[components]/MobileNav.tsx
+++ pages/[components]/MobileNav.tsx
@@ -12,20 +12,20 @@ const navAnimation = Nav?'translate-x-0':'translate-x-[-100%]';
   return ( 
     <div
      className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[10000]
-      bg-black`}>
+      `}>
 
-       <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center'>
-       <Link href="./"><div className='nav-link'>Home</div></Link>
-   <Link href="/Components/About"><div className='nav-link'>About</div></Link>
-   <Link href="/Components/Services"><div className='nav-link'>Services</div></Link>
-   <Link href="/Components/Skills"><div className='nav-link'>Educatio & Skills</div></Link>
-   <Link href="./"><div className='nav-link'>Blogs</div></Link>
-   <Link href="/Components/Skills"><div className='nav-link'>Contact</div></Link>
+       <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center bg-blue-950'>
+       <Link href="./"><div className='nav-link-mobile'>Home</div></Link>
+   <Link href="/Components/About"><div className='nav-link-mobile'>About</div></Link>
+   <Link href="/Components/Services"><div className='nav-link-mobile'>Services</div></Link>
+   <Link href="/Components/Skills"><div className='nav-link-mobile'>Educatio & Skills</div></Link>
+   <Link href="./"><div className='nav-link-mobile'>Blogs</div></Link>
+   <Link href="/Components/Skills"><div className='nav-link-mobile'>Contact</div></Link>
       
 </div>
 <div
  onClick={closeNavbar}
- className='absolute z-[100000000] cursor-pointer top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-yellow-500'>
+ className='absolute z-[100000000] cursor-pointer top-[4rem] right-[2rem] w-[2rem] h-[2rem] text-yellow-500'>
   <XMarkIcon/>
 </div>
        
